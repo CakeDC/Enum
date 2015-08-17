74fa637134f5a04c63a643bf387e2d21a80bb430
Jad Bitar <jadbitar@mac.com>
2015-08-17 11:06:37 -0400

    Add missing migration, closes #2

---
+++ config/Migrations/001_create_lookups.php
@@ -0,0 +1,35 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class CreateEnumLookups extends AbstractMigration
+{
+    /**
+     * Creates the `enum_lookups` table.
+     *
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('enum_lookups');
+
+        $table->addColumn('label', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+
+        $table->addColumn('prefix', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+
+        $table->create();
+    }
+}
