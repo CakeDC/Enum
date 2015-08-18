72566b7a2888928f90e1499b212bbf90a804cb16
Jad Bitar <jadbitar@mac.com>
2015-08-18 14:21:07 -0400

    Update fixtures

--- tests/Fixture/ArticlesFixture.php
+++ tests/Fixture/ArticlesFixture.php
@@ -11,15 +11,16 @@ class ArticlesFixture extends TestFixture
         'id' => ['type' => 'integer'],
         'title' => ['type' => 'string'],
         'body' => ['type' => 'text'],
-        'priority' => ['type' => 'string'],
-        'status' => ['type' => 'string'],
-        'article_category' => ['type' => 'string'],
+        'priority' => ['type' => 'integer'],
+        'status' => ['type' => 'integer'],
+        'article_category' => ['type' => 'integer'],
         '_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']]],
         '_options' => ['engine' => 'InnoDB', 'collation' => 'utf8_general_ci'],
     ];
 
     public $records = [
         [
+            'id' => 1,
             'title' => 'Dummy article',
             'body' => '',
             'priority' => 'HIGH',
--- tests/Fixture/LookupsFixture.php
+++ tests/Fixture/LookupsFixture.php
@@ -16,41 +16,49 @@ class LookupsFixture extends TestFixture
 
     public $records = [
         [
+            'id' => 1,
             'label' => 'Urgent',
             'prefix' => 'PRIORITY',
             'name' => 'URGENT',
         ],
         [
+            'id' => 2,
             'label' => 'High',
             'prefix' => 'PRIORITY',
             'name' => 'HIGH',
         ],
         [
+            'id' => 3,
             'label' => 'Normal',
             'prefix' => 'PRIORITY',
             'name' => 'NORMAL',
         ],
         [
+            'id' => 4,
             'label' => 'Published',
             'prefix' => 'ARTICLE_STATUS',
             'name' => 'PUBLIC',
         ],
         [
+            'id' => 5,
             'label' => 'Drafted',
             'prefix' => 'ARTICLE_STATUS',
             'name' => 'DRAFT',
         ],
         [
+            'id' => 6,
             'label' => 'Archived',
             'prefix' => 'ARTICLE_STATUS',
             'name' => 'ARCHIVE',
         ],
         [
+            'id' => 7,
             'label' => 'CakePHP',
             'prefix' => 'ARTICLE_CATEGORY',
             'name' => 'CAKEPHP',
         ],
         [
+            'id' => 8,
             'label' => 'Open Source Software',
             'prefix' => 'ARTICLE_CATEGORY',
             'name' => 'OSS',
