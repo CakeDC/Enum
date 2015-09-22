84922bded366a873b8a83024ce96476ed5dd68fe
Jad Bitar <jadbitar@mac.com>
2015-09-22 11:27:40 -0400

    Fix omitted copyright date

--- src/Model/Entity/Lookup.php
+++ src/Model/Entity/Lookup.php
@@ -6,7 +6,7 @@
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright 2010 - 2015, Cake Development Corporation (http://cakedc.com)
+ * @copyright Copyright 2015, Cake Development Corporation (http://cakedc.com)
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
