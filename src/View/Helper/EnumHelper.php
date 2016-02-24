363a36eaafd01e27d0204b64224d9908a0529f46
Peter Moraes <peter.moraes@cakedc.com>
2016-02-23 22:59:02 -0300

    fixing code standard

--- src/View/Helper/EnumHelper.php
+++ src/View/Helper/EnumHelper.php
@@ -7,80 +7,92 @@ use Cake\ORM\TableRegistry;
 /**
  * Enumeration List Helper.
  */
-class EnumHelper extends Helper {
+class EnumHelper extends Helper
+{
 
-	private $className;
+    private $_className;
 
-	/**
-	 * Helper dependencies
-	 *
-	 * @var array
-	 */
-	public $helpers = ['Form'];
+    /**
+     * Helper dependencies
+     *
+     * @var array
+     */
+    public $helpers = ['Form'];
 
-	/**
-	 * Get Enumeration list
-	 *
-	 * @param string $fieldName This should be "modelname.fieldname"
-	 * @param string $tableName if fieldName contain "modelname", it not need to be passed.
-     * @param array $options Each type of input takes different options, here should be passed the prefix and the strategy
+    /**
+     * Get Enumeration list
+     *
+     * @param string $fieldName This should be "modelname.fieldname"
+     * @param string $tableName if fieldName contain "modelname", 
+     *   it not need to be passed.
+     * @param array $options Each type of input takes different options,
+     *   here should be passed the prefix and the strategy
      * @throws RuntimeException
-	 * @return array
-	 */
-	public function input($fieldName, $tableName = '', $options = []) {
-		if (strpos($fieldName, '.') !== false && empty($tableName)) {
-			$name = explode('.', $fieldName);
-			$tableName = reset($name);
-		}
+     * @return array
+     */
+    public function input($fieldName, $tableName = '', $options = [])
+    {
+        if (strpos($fieldName, '.') !== false && empty($tableName)) {
+            $name = explode('.', $fieldName);
+            $tableName = reset($name);
+        }
 
-		if (!isset($options['alias']) && empty($options['alias'])) {
-			throw new \RuntimeException('You need to set the alias defined in the table!');
-		}
-		$strategy = $options['alias'];
-		unset($options['alias']);
-		$Table = $this->tableInstance($tableName);
-		$result = $Table->enum($strategy);
-		$options['options'] = $result;
-		return $this->Form->input($fieldName, $options);
-	}
+        if (!isset($options['alias']) && empty($options['alias'])) {
+            throw new \RuntimeException(
+                'You need to set the alias defined in the table!'
+            );
+        }
+        $strategy = $options['alias'];
+        unset($options['alias']);
+        $Table = $this->_tableInstance($tableName);
+        $result = $Table->enum($strategy);
+        $options['options'] = $result;
+        return $this->Form->input($fieldName, $options);
+    }
 
-	/**
-	 * Get instance of the a strategy
-	 *
-	 * @param string $tableName if fieldName contain "modelname", it not need to be passed.
-	 * @throws RuntimeException
-	 * @return Object
-	 */
-	private function tableInstance($tableName) {
-		$tableName = ucfirst($tableName);
+    /**
+     * Get instance of the a strategy
+     *
+     * @param string $tableName if fieldName contain "modelname",
+     *   it not need to be passed.
+     * @throws RuntimeException
+     * @return Object
+     */
+    private function _tableInstance($tableName)
+    {
+        $tableName = ucfirst($tableName);
 
-		if (empty($this->className)) {
-			$class = TableRegistry::get($tableName);
-		} else {
-			$class = TableRegistry::get($tableName, [
-				'className' => $this->getClassName()
-			]);
-		}
+        if (empty($this->getClassName())) {
+            $class = TableRegistry::get($tableName);
+        } else {
+            $class = TableRegistry::get($tableName,
+                [
+                    'className' => $this->getClassName()
+                ]
+            );
+        }
 
-		return $class;
-	}
+        return $class;
+    }
 
-	/**
-	 * Setting class name to put the real place of the class
-	 *
-	 * @param string $path path of the class
-	 * @return void
-	 */
-	public function setClassName($className) {
-		$this->className = $className;
-	}
+    /**
+     * Setting class name to put the real place of the class
+     *
+     * @param string $className path of the class
+     * @return void
+     */
+    public function setClassName($className)
+    {
+        $this->_className = $className;
+    }
 
-	/**
-	 * Getting class name
-	 *
-	 * @return string
-	 */
-	public function getClassName() {
-		return $this->className;
-	}
+    /**
+     * Getting class name
+     *
+     * @return string
+     */
+    public function getClassName()
+    {
+        return $this->_className;
+    }
 }
